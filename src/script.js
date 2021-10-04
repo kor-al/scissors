30fd4426980a9341e3ed2621a3dd45550c6446fb
enantiolis <korinalice@gmail.com>
2021-10-04 21:53:29 +0300

    deploy

--- src/script.js
+++ src/script.js
@@ -2,8 +2,8 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import waterVertexShader from "./shaders/water/vertex.glsl";
-import waterFragmentShader from "./shaders/water/fragment.glsl";
+//import waterVertexShader from "./shaders/water/vertex.glsl";
+//import waterFragmentShader from "./shaders/water/fragment.glsl";
 import noiseVertexShader from "./shaders/noise/vertex.glsl";
 import noiseFragmentShader from "./shaders/noise/fragment.glsl";
 import { gsap } from "gsap";
@@ -53,7 +53,7 @@ const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager);
 // Debug
 const gui = new dat.GUI();
 const debugObject = {};
-debugObject.background = 0xf1f1f1;
+debugObject.background = 0x0;
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -138,101 +138,101 @@ gui
 //const waterGeometry = new THREE.PlaneBufferGeometry(2, 2, 512, 512);
 
 // Colors
-debugObject.depthColor = "#186691";
-debugObject.surfaceColor = "#9bd8ff";
+// debugObject.depthColor = "#186691";
+// debugObject.surfaceColor = "#9bd8ff";
 
-gui.addColor(debugObject, "depthColor").onChange(() => {
-  waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
-});
-gui.addColor(debugObject, "surfaceColor").onChange(() => {
-  waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
-});
+// gui.addColor(debugObject, "depthColor").onChange(() => {
+//   waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
+// });
+// gui.addColor(debugObject, "surfaceColor").onChange(() => {
+//   waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
+// });
 
-// Material
-const waterMaterial = new THREE.ShaderMaterial({
-  vertexShader: waterVertexShader,
-  fragmentShader: waterFragmentShader,
-  uniforms: {
-    uTime: { value: 0 },
+// // Material
+// const waterMaterial = new THREE.ShaderMaterial({
+//   vertexShader: waterVertexShader,
+//   fragmentShader: waterFragmentShader,
+//   uniforms: {
+//     uTime: { value: 0 },
 
-    uBigWavesElevation: { value: 0.2 },
-    uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
-    uBigWavesSpeed: { value: 0.75 },
+//     uBigWavesElevation: { value: 0.2 },
+//     uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+//     uBigWavesSpeed: { value: 0.75 },
 
-    uSmallWavesElevation: { value: 0.15 },
-    uSmallWavesFrequency: { value: 3 },
-    uSmallWavesSpeed: { value: 0.2 },
-    uSmallIterations: { value: 4 },
+//     uSmallWavesElevation: { value: 0.15 },
+//     uSmallWavesFrequency: { value: 3 },
+//     uSmallWavesSpeed: { value: 0.2 },
+//     uSmallIterations: { value: 4 },
 
-    uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
-    uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
-    uColorOffset: { value: 0.08 },
-    uColorMultiplier: { value: 5 },
-  },
-});
+//     uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+//     uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+//     uColorOffset: { value: 0.08 },
+//     uColorMultiplier: { value: 5 },
+//   },
+// });
 
-gui
-  .add(waterMaterial.uniforms.uBigWavesElevation, "value")
-  .min(0)
-  .max(1)
-  .step(0.001)
-  .name("uBigWavesElevation");
-gui
-  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
-  .min(0)
-  .max(10)
-  .step(0.001)
-  .name("uBigWavesFrequencyX");
-gui
-  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
-  .min(0)
-  .max(10)
-  .step(0.001)
-  .name("uBigWavesFrequencyY");
-gui
-  .add(waterMaterial.uniforms.uBigWavesSpeed, "value")
-  .min(0)
-  .max(4)
-  .step(0.001)
-  .name("uBigWavesSpeed");
+// gui
+//   .add(waterMaterial.uniforms.uBigWavesElevation, "value")
+//   .min(0)
+//   .max(1)
+//   .step(0.001)
+//   .name("uBigWavesElevation");
+// gui
+//   .add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
+//   .min(0)
+//   .max(10)
+//   .step(0.001)
+//   .name("uBigWavesFrequencyX");
+// gui
+//   .add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
+//   .min(0)
+//   .max(10)
+//   .step(0.001)
+//   .name("uBigWavesFrequencyY");
+// gui
+//   .add(waterMaterial.uniforms.uBigWavesSpeed, "value")
+//   .min(0)
+//   .max(4)
+//   .step(0.001)
+//   .name("uBigWavesSpeed");
 
-gui
-  .add(waterMaterial.uniforms.uSmallWavesElevation, "value")
-  .min(0)
-  .max(1)
-  .step(0.001)
-  .name("uSmallWavesElevation");
-gui
-  .add(waterMaterial.uniforms.uSmallWavesFrequency, "value")
-  .min(0)
-  .max(30)
-  .step(0.001)
-  .name("uSmallWavesFrequency");
-gui
-  .add(waterMaterial.uniforms.uSmallWavesSpeed, "value")
-  .min(0)
-  .max(4)
-  .step(0.001)
-  .name("uSmallWavesSpeed");
-gui
-  .add(waterMaterial.uniforms.uSmallIterations, "value")
-  .min(0)
-  .max(5)
-  .step(1)
-  .name("uSmallIterations");
+// gui
+//   .add(waterMaterial.uniforms.uSmallWavesElevation, "value")
+//   .min(0)
+//   .max(1)
+//   .step(0.001)
+//   .name("uSmallWavesElevation");
+// gui
+//   .add(waterMaterial.uniforms.uSmallWavesFrequency, "value")
+//   .min(0)
+//   .max(30)
+//   .step(0.001)
+//   .name("uSmallWavesFrequency");
+// gui
+//   .add(waterMaterial.uniforms.uSmallWavesSpeed, "value")
+//   .min(0)
+//   .max(4)
+//   .step(0.001)
+//   .name("uSmallWavesSpeed");
+// gui
+//   .add(waterMaterial.uniforms.uSmallIterations, "value")
+//   .min(0)
+//   .max(5)
+//   .step(1)
+//   .name("uSmallIterations");
 
-gui
-  .add(waterMaterial.uniforms.uColorOffset, "value")
-  .min(0)
-  .max(1)
-  .step(0.001)
-  .name("uColorOffset");
-gui
-  .add(waterMaterial.uniforms.uColorMultiplier, "value")
-  .min(0)
-  .max(10)
-  .step(0.001)
-  .name("uColorMultiplier");
+// gui
+//   .add(waterMaterial.uniforms.uColorOffset, "value")
+//   .min(0)
+//   .max(1)
+//   .step(0.001)
+//   .name("uColorOffset");
+// gui
+//   .add(waterMaterial.uniforms.uColorMultiplier, "value")
+//   .min(0)
+//   .max(10)
+//   .step(0.001)
+//   .name("uColorMultiplier");
 
 // // Mesh
 // const water = new THREE.Mesh(waterGeometry, waterMaterial)
@@ -386,21 +386,6 @@ for (let i = 0; i < modelsParams.files.length; i++) {
 }
 //group.rotation.z = -Math.PI / 8;
 scene.add(group);
-
-// gltfLoader.load("/models/tailorShears.glb", (gltf) => {
-//     const model = gltf.scene;
-//     model.scale.set(2.5, 2.5, 2.5);
-//     model.rotation.y = Math.PI * 0.5;
-//     console.log(model);
-//     for (let obj of initMaterialMap) {
-//       assignMaterial(model, obj.objectID, obj.material);
-//     }
-
-//     scene.add(gltf.scene);
-
-//     updateAllMaterials();
-//   });
-
 
 
 /**
@@ -641,8 +626,9 @@ const tick = () => {
 
   const elapsedTime = clock.getElapsedTime();
   const radiansPerSecond = 0.5;
-  // Water
-  waterMaterial.uniforms.uTime.value = elapsedTime;
+
+  
+  //waterMaterial.uniforms.uTime.value = elapsedTime;
   noiseMaterial.uniforms.uTime.value = elapsedTime;
 
   //Rotate
